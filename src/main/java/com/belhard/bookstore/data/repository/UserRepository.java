aa1def8a210a7ef9a65a766a6be03400b1793755
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-02-03 20:03:55 +0300

    Added UserRepository

---
+++ src/main/java/com/belhard/bookstore/data/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.belhard.bookstore.data.repository;
+
+import com.belhard.bookstore.data.entity.User;
+
+import java.util.List;
+
+public interface UserRepository{
+    User create(User user);
+    User findById(Long id);
+    User findByEmail(String email);
+    List<User> findByLastName(String lastName);
+    List<User> findAll();
+    User update(User user);
+    boolean delete(Long id);
+}
---
+++ src/main/java/com/belhard/bookstore/data/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.belhard.bookstore.data.repository.impl;
+
+import com.belhard.bookstore.data.dao.UserDao;
+import com.belhard.bookstore.data.dto.UserDto;
+import com.belhard.bookstore.data.entity.User;
+import com.belhard.bookstore.data.repository.UserRepository;
+import com.belhard.bookstore.data.util.DataMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+    private final UserDao userDao;
+    private final DataMapper dataMapper;
+    @Override
+    public User create(User user) {
+        UserDto userDto = dataMapper.toDto(user);
+        UserDto created = userDao.create(userDto);
+        return dataMapper.toEntity(created);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return dataMapper.toEntity(userDao.findById(id));
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return dataMapper.toEntity(userDao.findByEmail(email));
+    }
+
+    @Override
+    public List<User> findByLastName(String lastName) {
+        return dataMapper.toUsersEntity(userDao.findByLastName(lastName));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return dataMapper.toUsersEntity(userDao.findAll());
+    }
+
+    @Override
+    public User update(User user) {
+        UserDto userDto = dataMapper.toDto(user);
+        UserDto updated = userDao.update(userDto);
+        return dataMapper.toEntity(updated);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return userDao.delete(id);
+    }
+}
