3ce0a50d213b6c73fc70cf744f267967fc934be0
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-02-06 21:56:40 +0300

    Added object OrderDto

---
+++ src/main/java/com/belhard/bookstore/data/dto/OrderInfoDto.java
@@ -0,0 +1,13 @@
+package com.belhard.bookstore.data.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class OrderInfoDto {
+    private Long id;
+    private Long orderId;
+    private Long bookId;
+    private Integer bookQuantity;
+    private BigDecimal bookPrice;
+}
