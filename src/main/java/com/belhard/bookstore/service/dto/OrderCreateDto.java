bb8eac14514b2df3230d469f087a3ffc18704348
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-02-07 18:51:16 +0300

    Added OrderDto

---
+++ src/main/java/com/belhard/bookstore/service/dto/OrderCreateDto.java
@@ -0,0 +1,17 @@
+package com.belhard.bookstore.service.dto;
+
+import com.belhard.bookstore.data.entity.OrderInfo;
+import com.belhard.bookstore.data.entity.User;
+import com.belhard.bookstore.data.entity.enums.Status;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class OrderCreateDto {
+    private User user;
+    private BigDecimal totalCost;
+    private Status status;
+    private List<OrderInfo> items;
+}
