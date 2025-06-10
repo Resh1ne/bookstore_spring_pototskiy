3b5e2f67fadad70f66b6b0fc75fa0e183c0b6236
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-06-10 23:30:04 +0300

    Added view resolver bean

--- src/main/java/com/belhard/bookstore/AppContext.java
+++ src/main/java/com/belhard/bookstore/AppContext.java
@@ -8,11 +8,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
 public class AppContext {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
     @Bean
     public EntityManagerFactory entityManagerFactory() {
         return Persistence.createEntityManagerFactory("psql");
