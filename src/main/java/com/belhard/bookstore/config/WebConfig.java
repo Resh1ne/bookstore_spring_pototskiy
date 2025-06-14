b49e361fb76dcdd05c346c439b65ad914ef150bd
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-06-15 00:34:52 +0300

    Added configuration for localization

--- src/main/java/com/belhard/bookstore/config/WebConfig.java
+++ src/main/java/com/belhard/bookstore/config/WebConfig.java
@@ -3,18 +3,39 @@ package com.belhard.bookstore.config;
 import com.belhard.bookstore.web.interceptor.MyInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(myInterceptor());
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
     @Bean
     public MyInterceptor myInterceptor() {
         return new MyInterceptor();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setLocaleAttributeName("locale");
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
 }
