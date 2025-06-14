fbedbccd6d47c533a5bb02214db747e01848f705
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-06-15 01:01:13 +0300

    Added validation constraints to UserDto

--- src/main/java/com/belhard/bookstore/service/dto/UserDto.java
+++ src/main/java/com/belhard/bookstore/service/dto/UserDto.java
@@ -1,8 +1,11 @@
 package com.belhard.bookstore.service.dto;
 
-import com.belhard.bookstore.data.entity.User;
 import com.belhard.bookstore.data.entity.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UserDto {
@@ -10,7 +13,14 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private Role role;
+
+    @NotBlank(message = "{validation.user.email.null}")//will be localized by {code{
+    @Email(message = "{validation.user.email.type}")
     private String email;
+
+    @NotBlank //Custom localized message should be used instead of default
+    @Length(min = 8, message = "{validation.user.password.min}")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!_]).{8,}$", message = "{validation.user.password.regexp}")
     private String password;
     private Integer age;
 }
