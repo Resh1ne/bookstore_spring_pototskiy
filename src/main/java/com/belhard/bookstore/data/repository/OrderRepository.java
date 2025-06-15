6220d1877a2bed0dd88cd13ea3a4807ce9fc0afc
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-06-16 00:00:23 +0300

    Changed repositories

--- src/main/java/com/belhard/bookstore/data/repository/OrderRepository.java
+++ src/main/java/com/belhard/bookstore/data/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.belhard.bookstore.data.repository;
 
 import com.belhard.bookstore.data.entity.Order;
+import com.belhard.bookstore.data.entity.User;
+import com.belhard.bookstore.data.entity.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserId(Long userId);
+    List<Order> findByUser(User user);
+    Optional<Order> findByUserAndStatus(User user, Status status);
 }
